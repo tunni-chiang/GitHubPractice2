daedc59c8747f03a4bf9d6495bbae2c8aaab0be2
Tunni Chiang <tchiang3@mail.sfsu.edu>
2021-10-31 20:57:52 -0700

    updated and added branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is how you create a new repository!");
+        System.out.println("This line is added from test branch, and hopefully can merge to main");
     }
 }
